5058b85b81cbf36b69a850ed78f228133843b1ac
Gabriel Silva <gabrielp_lacerda@hotmail.com>
2023-05-25 20:56:16 -0300

    aula de api

---
+++ javascript/javascript-assincrono/animais-fetch/js/modules/fetch-bitcoin.js
@@ -0,0 +1,14 @@
+export default function initFetchBitcoin() {
+    async function fetchBitcoin(url) {
+        try {
+            const bitcoinResponse = await fetch(url);
+            const bitcoinJson = await bitcoinResponse.json();
+            const btc = document.querySelector('.btc-preco')
+            btc.innerText = (1000 / bitcoinJson.BRL.sell).toFixed(4);
+        } catch (erro){
+            alert(Error(erro));
+        } 
+    };
+
+    fetchBitcoin('https://blockchain.info/ticker');
+};
